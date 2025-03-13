b3ba567a1f4919cb2c1e9be23d989ad36440cd29
Rithvik <rithvik.utd@outlook.com>
2025-03-13 12:02:04 -0400

    Initial Commit

---
+++ SwiftUIRefresher/SwiftUIRefresherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIRefresherApp.swift
+//  SwiftUIRefresher
+//
+//  Created by Rithvik Pallamreddy on 3/13/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIRefresherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
