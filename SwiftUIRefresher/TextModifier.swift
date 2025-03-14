4f658e2a37903ea9ea65b764d433d7fe51b31cff
Rithvik <rithvik.utd@outlook.com>
2025-03-13 21:20:07 -0400

    Text Modifier

---
+++ SwiftUIRefresher/TextModifier.swift
@@ -0,0 +1,18 @@
+//
+//  Text.swift
+//  SwiftUIRefresher
+//
+//  Created by Rithvik Pallamreddy on 3/13/25.
+//
+
+import SwiftUI
+
+struct Text: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Text()
+}
